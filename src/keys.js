242c096658233542423c9002edf1d1b588c52247
Eduardo_Chavez <hector.eduardo.godinez.chavez@gmail.com>
2021-08-20 14:59:21 -0500

    Configuracion database

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'registroDepartamento'
+        host: 'bn1nj3yrnt9qwqz2ejwr-mysql.services.clever-cloud.com',
+        user: 'uk71t7wkrn92ui0x',
+        password: 'Hssk5Nrrx2zUSWDPXr6o',
+        database: 'bn1nj3yrnt9qwqz2ejwr'
     }
 
 };
